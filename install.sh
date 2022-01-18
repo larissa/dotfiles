c6d9bca320ea4fcc570fc71b7953ca0ce1b7ddf8
Larissa Reis <reiss.larissa@gmail.com>
2022-01-17 22:22:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,11 @@ cat gitignore >> ~/.gitignore
 ln -s -f $cwd/git_template ~/.git_template
 ln -s -f $cwd/ripgreprc ~/.ripgreprc
 ln -s -f $cwd/tigrc ~/.tigrc
+ln -s -f $cwd/tmux.conf ~/.tmux.conf
+
+if [ SPIN ]; then
+  sed -i '/fish\|kitty\|smxx/d' tmux.conf
+fi
 
 git clone --depth 1 https://github.com/larissa/vim-configs.git ~/.vim
 cd ~/.vim
