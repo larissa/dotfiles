ffbf945d10d3e2d307ac6fd61da286abc4ce9512
Larissa Reis <larissa.reis@shopify.com>
2022-10-04 17:40:37 -0400

    Remove spin hack from install script

--- install.sh
+++ install.sh
@@ -15,14 +15,7 @@ ln -s -f $cwd/ripgreprc ~/.ripgreprc
 ln -s -f $cwd/tigrc ~/.tigrc
 ln -s -f $cwd/tmux.conf ~/.tmux.conf
 
-if [ $SPIN ]; then
-  sed -i '/fish\|kitty\|smxx/d' tmux.conf
-fi
-
 git clone --depth 1 https://github.com/larissa/vim-configs.git ~/.vim
 cd ~/.vim
 ./install.sh
-if [ $SPIN ]; then
-  echo "let g:ruby_host_prog = '"`gem environment | awk '/EXECUTABLE DIRECTORY/ {print $4}'`"'" >> vimrc
-fi
 cd $cwd
