868ba05c6ebbf5c7044eaff3926889052d0e5065
Larissa Reis <larissa.reis@shopify.com>
2022-10-05 14:00:14 -0400

    Fix git_open function on spin

--- fish/functions/git_open.fish
+++ fish/functions/git_open.fish
@@ -3,6 +3,9 @@ function git_open -d "Open file on github with browser"
   if test (uname -s) = 'Darwin'
     set browser_command 'open'
   end
+  if set -q SPIN
+    set browser_command 'open'
+  end
   set -l host 'https://github.com/'
   set -l branch 'blob/main/'
   set -l filepath $argv[1]
