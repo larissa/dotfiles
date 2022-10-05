11cd4b46c180392dcf2f90bb8cd72aa5141a0cb2
Larissa Reis <larissa.reis@shopify.com>
2022-10-05 11:48:23 -0400

    Ignore error if github token is missing

--- fish/conf.d/personal.fish
+++ fish/conf.d/personal.fish
@@ -3,7 +3,7 @@ set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 set -gx FZF_DEFAULT_COMMAND 'rg --files'
 set -gx FZF_DEFAULT_OPTS '--tiebreak=end,length,index'
 
-set -gx GITHUB_TOKEN (sed 's/@.*//' /opt/dev/var/private/git_credential_store | string split ':' -r -m1 -f2)
+set -gx GITHUB_TOKEN (sed 's/@.*//' /opt/dev/var/private/git_credential_store 2>/dev/null | string split ':' -r -m1 -f2 2>/dev/null)
 set -gx GLAMOUR_STYLE 'dark'
 
 set -g theme_short_path yes
