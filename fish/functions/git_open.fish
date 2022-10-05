d2c5b0813354529bb51aef15f5d834b6ae978d5a
Larissa Reis <larissa.reis@shopify.com>
2022-10-05 14:48:37 -0400

    Fix git_open on older fish versions
    string match with capture group to variable was failing the older fish
    version we have on spin.

--- fish/functions/git_open.fish
+++ fish/functions/git_open.fish
@@ -10,7 +10,9 @@ function git_open -d "Open file on github with browser"
   set -l branch 'blob/main/'
   set -l filepath $argv[1]
   set -l linenumber $argv[2]
-  string match -rq 'github.com/(?<repo>[^/]*/[^/]*/)(?<file>.*)' $filepath
+  set matches (echo $filepath | sed -n 's/.*github.com\/\([^\/]*\/[^\/]*\/\)\(.*\)/\1\n\2/p')
+  set repo $matches[1]
+  set file $matches[2]
   command $browser_command "$host$repo$branch$file#L$linenumber"
 end
 
